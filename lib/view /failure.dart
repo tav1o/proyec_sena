56880edfffc3a7d55e784210973c7d78a63fff8e
Oadmin <oadmin@kali.admin>
2025-05-21 02:09:03 -0500

    realizamos la funcion de cada ventana y los archivos #creamos model y data, creamos el bloc

---
+++ lib/view /failure.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class failure extends StatelessWidget {
+  const failure({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: []));
+  }
+}
---
+++ lib/view /success.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class success extends StatelessWidget {
+  const success({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: []);
+  }
+}
