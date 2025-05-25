ac41358668189922ac1ef21dc9a43c690dc9a827
Oadmin <oadmin@kali.admin>
2025-05-25 16:23:13 -0500

    agregamos fondo en inicio

--- lib/view /initial.dart
+++ lib/view /initial.dart
@@ -2,87 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:proyec_sena/bloc/home_bloc.dart';
 
-class initial extends StatelessWidget {
-  const initial({
-    super.key,
-  });
+class Initial extends StatelessWidget {
+  const Initial({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text(
-        'Futbol Pro',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 20, 
-          ),  
-        ),
-
-      centerTitle: true,
-      
-      flexibleSpace: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors:[
-            Colors.blueGrey[700]!,
-            Colors.grey,
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        )
-      ),
-       leading: Icon(
-         Icons.sports_soccer,
-          color: Colors.white,
-          size: 30, 
-       ),
-          actions:[
-          IconButton( 
-            icon: Icon(Icons.stars,color: Colors.white, size: 30,),
-             onPressed: () => print("favoritos"),
-        ),
-       ],
-      ),
-        body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blueGrey[700]!, Colors.grey],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+        title: const Text(
+          'Futbol Pro',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
           ),
         ),
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-            "Bienvenido al Mundo del Futbol",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              shadows: [Shadow(blurRadius: 10, color: Colors.black)],
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueGrey, Colors.grey],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
           ),
-          
-              SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  // Disparar el evento para cargar los datos
-                  context.read<HomeBloc>().add(HomeSearchPressed());
-                },
-                child: const Text('buscar'),
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 15,
-                  ),
-                  textStyle: const TextStyle(fontSize: 18),
-                ),
-              ),
-            ],
-          ),
         ),
+        leading: const Icon(Icons.sports_soccer,
+          color: Colors.white,
+          size: 30),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.stars, color: Colors.white, size: 30),
+            onPressed: () => print("favoritos"),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          // Imagen de fondo
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/jugadores/fubol.png'), 
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          Container(color: Colors.black.withOpacity(0.5)),
+
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Bienvenido al Mundo del Futbol",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    shadows: [Shadow(blurRadius: 10, color: Colors.black)],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<HomeBloc>().add(HomeSearchPressed());
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 15,
+                    ),
+                    textStyle: const TextStyle(fontSize: 18),
+                    backgroundColor: Colors.orange[700],
+                  ),
+                  child: const Text('Buscar'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
